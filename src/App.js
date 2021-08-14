d7885b2b7d7b2a2bae81333cc6d7eb368ef1f46f
ayanhan <kalkhozhaev.j@gmail.com>
2021-08-15 00:05:22 +0900

    main layout finished

--- src/App.js
+++ src/App.js
@@ -1,27 +1,171 @@
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-          <img width={40} height={40} src="/img/logo.png" alt="logo" />
-          <div className="headerInfo">
-            <h3>React Sneakers</h3>
-            <p>Best sneakers shop</p>
+    <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30">Cart</h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Men's Nike Blazer Mid Suede</p>
+                <b>120$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Men's Nike Blazer Mid Suede</p>
+                <b>120$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Total:</span>
+                <div></div>
+                <b>125$</b>
+              </li>
+              <li>
+                <span>Tax 10%:</span>
+                <div></div>
+                <b>12$</b>
+              </li>
+            </ul>
+            <button>Confirm Order</button>
           </div>
         </div>
-        <ul className="headerRight">
-          <li>
-          <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+      </div>
+
+      <header className="d-flex justify-between align center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/img.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Best sneakers shop</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
             <span>120$</span>
           </li>
           <li>
-          <img width={18} height={18} src="/img/user.png" alt="user" />
+            <img width={18} height={18} src="/img/user.png" alt="user" />
           </li>
         </ul>
       </header>
-      <div className="content">
-        <h1>Sneakers</h1>
-        ...
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Sneakers</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Search..." />
+          </div>
+        </div>
+
+        <div className="d-flex">
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
+            <img
+              width={133}
+              height={112}
+              src="img/sneakers/1.jpg"
+              alt="sneaker"
+            />
+            <h5>Men's Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price</span>
+                <b>180$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="img/sneakers/2.jpg"
+              alt="sneaker"
+            />
+            <h5>Men's Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price</span>
+                <b>180$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="img/sneakers/3.jpg"
+              alt="sneaker"
+            />
+            <h5>Men's Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price</span>
+                <b>180$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="img/sneakers/4.jpg"
+              alt="sneaker"
+            />
+            <h5>Men's Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price</span>
+                <b>180$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
